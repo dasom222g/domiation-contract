9970204f131ad866e401f336fcf9b67a9b43be89
dasom <dasom228@gmail.com>
2022-03-19 23:09:28 +0900

    Add tokenURI

--- Donation.sol
+++ Donation.sol
@@ -3,9 +3,21 @@
 pragma solidity ^0.8.0; // 컴파일러 버전
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+// import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract Donation is ERC721Enumerable {
-  constructor() ERC721("Donation", "SDN") {}
+  string uri;
+
+  mapping(uint256 => string) tokenURIs;
+
+  constructor(string memory _uri) ERC721("Donation", "SDN") {
+    uri = _uri;
+  }
+
+  function tokenURI(uint256 _tokenId) override public view returns(string memory) {
+    // string memory test = Strings.toString(_tokenId); // uint를 string으로 변환
+    return string(abi.encodePacked(uri, '/', _tokenId, '.json'));
+  }
 
   function mintDonation() public {
     uint tokenId = totalSupply() + 1;  
