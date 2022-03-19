3b345b0cb0cc090d07e9056db16a07a79340ef32
dasom <dasom228@gmail.com>
2022-03-20 00:35:58 +0900

    Fix tokenURI toString

--- Donation.sol
+++ Donation.sol
@@ -8,19 +8,17 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 contract Donation is ERC721Enumerable {
   string uri;
 
-  mapping(uint256 => string) tokenURIs;
-
-  constructor(string memory _uri) ERC721("Donation", "SDN") {
+  constructor(string memory _uri) ERC721("DonationTest", "SDN") {
     uri = _uri;
   }
 
   function tokenURI(uint256 _tokenId) override public view returns(string memory) {
-    // string memory test = Strings.toString(_tokenId); // uint를 string으로 변환
-    return string(abi.encodePacked(uri, '/', _tokenId, '.json'));
+    string memory stringTokenId = Strings.toString(_tokenId); // uint를 string으로 변환
+    return string(abi.encodePacked(uri, '/', stringTokenId, '.json'));
   }
 
   function mintDonation() public {
-    uint tokenId = totalSupply() + 1;  
+    uint256 tokenId = totalSupply() + 1;
     _mint(msg.sender, tokenId);
   }
 }
